a2ca006087961c5187b12b317f3009d6bdffdd86
Pickle Dan <PickleDan@yandex.ru>
2020-02-02 14:57:24 +0300

    add API OpenWeather

--- WeatherApp/WeatherApp/Views/CurrentWeatherPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/CurrentWeatherPage.xaml.cs
@@ -1,20 +1,60 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherApp.Helper;
+using WeatherApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WeatherApp.Views
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CurrentWeatherPage : ContentPage
     {
         public CurrentWeatherPage()
         {
             InitializeComponent();
+            GetWeatherInfo();
+        }
+
+        private string Location = "Pestretsy";
+
+        private async void GetWeatherInfo()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&APPID=a60f6152c2def05600769614aed2457d&units=metric";
+
+            var result = await ApiCaller.Get(url);
+
+            if (result.Successful)
+            {
+                try
+                {
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+                    descriptionTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    iconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+                    humidityTxt.Text = $"{weatherInfo.main.humidity}%";
+                    pressureTxt.Text = $"{weatherInfo.main.pressure} hpa";
+                    windTxt.Text = $"{weatherInfo.wind.speed} m/s";
+                    cloudinessTxt.Text = $"{weatherInfo.clouds.all}%";
+
+                    var dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            } else
+            {
+                await DisplayAlert("Weather Info", "No weather information found", "OK");
+            }
         }
     }
 }
